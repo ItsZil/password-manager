3c06f0df7535112bc7316aae86911152239b52d1
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-12-25 23:08:09 +0200

    Add SQLite and Entity Framework Core

---
+++ Server/Models/TestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; } = "Hello World";
+    }
+}
---
+++ Server/SqlContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server
+{
+    public class SqlContext : DbContext
+    {
+        public DbSet<TestModel> TestModels { get; set; }
+
+        public string DbPath { get; }
+
+        public SqlContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "database.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
