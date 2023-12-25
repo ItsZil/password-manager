e786944f97fcf75039bfd3a5a72fcf24f29a2b63
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-12-25 18:35:40 +0200

    Add some basic UI

--- BrowserExtension/background.js
+++ BrowserExtension/background.js
@@ -1,6 +1,26 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Password Manager extension installed.');
-  // Initialize extension state and set up communication with the local server here.
+    console.log('Password Manager extension installed.');
+
+    const serverUrl = 'http://localhost:5000'; // Replace with your server URL
+    const apiEndpoint = '/api/passwords'; // Replace with your API endpoint
+    const enableTest = false;
+
+    // Example function to communicate with the server
+    function fetchPasswords() {
+        if (enableTest) {
+            fetch(`${serverUrl}${apiEndpoint}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Passwords fetched from server:', data);
+                })
+                .catch(error => {
+                    console.error('Error fetching passwords:', error);
+                });
+        }
+    }
+
+    // Call the function to fetch passwords after installation
+    fetchPasswords();
 });
--- BrowserExtension/popup.js
+++ BrowserExtension/popup.js
@@ -1,5 +1,10 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Add your popup script logic here
+  // Placeholder logic for popup interaction
+  const fetchButton = document.getElementById('fetch-passwords');
+  fetchButton.addEventListener('click', () => {
+    // Placeholder for fetching passwords from the server
+    console.log('Fetch passwords button clicked');
+  });
 });
