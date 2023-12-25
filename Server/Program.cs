263d53ea7f123eaaaf911c20a03e69a5241fcbb5
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2023-12-25 19:05:53 +0200

    Add security middle-ware, restricting to local network

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace Server
 {
@@ -13,9 +15,41 @@ namespace Server
                 options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
             });
 
+            // Configure the app to serve over HTTPS only
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.ConfigureHttpsDefaults(httpsOptions =>
+                {
+                });
+            });
+
             var app = builder.Build();
 
             app.UseHttpsRedirection();
+
+            // Add HSTS middleware
+            app.UseHsts();
+
+            // Configure Forwarded Headers to allow for correct scheme usage behind reverse proxies
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            // Middleware to limit access to the local network
+            app.Use(async (context, next) =>
+            {
+                var remoteIp = context.Connection.RemoteIpAddress;
+                if (remoteIp != null && (remoteIp.Equals(IPAddress.Loopback) || remoteIp.Equals(IPAddress.IPv6Loopback) && context.Request.IsHttps))
+                {
+                    await next.Invoke();
+                }
+                else
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    await context.Response.WriteAsync("Access denied. This service is only available within the local network.");
+                }
+            });
 
             var testApi = app.MapGroup("/test");
             testApi.MapGet("/", () => $"Current time is {DateTime.Now}");
