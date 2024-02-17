43a8d9c389688860dcc85422659977b5c92914cb
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2024-02-17 22:02:47 +0200

    Add model files

---
+++ Server/Models/Authenticator.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models
+{
+    public class Authenticator
+    {
+        [Key]
+        public int AuthenticatorId { get; set; }
+        public int UserId { get; set; }
+        public string Type { get; set; } // TOTP, HOTP, etc.
+        public string Secret { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
