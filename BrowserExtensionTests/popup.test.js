3015281c073a2f44056369bc223cb132bbe9a2ee
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2024-03-03 22:12:02 +0200

    Fix popup tests

---
+++ BrowserExtensionTests/popup.test.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+const EXTENSION_PATH = path.resolve(__dirname, '../BrowserExtension');
+const EXTENSION_ID = 'cbloejcjdeplkphdlnkiehcfligdheje';
+
+let browser;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: "new",
+        args: [
+            `--disable-extensions-except=${EXTENSION_PATH}`,
+            `--load-extension=${EXTENSION_PATH}`
+        ]
+    });
+});
+
+afterEach(async () => {
+    await browser.close();
+    browser = undefined;
+});
+
+describe('Popup Tests', () => {
+    test('popup renders correctly', async () => {
+        const page = await browser.newPage();
+        await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+    });
+
+    test('popup contains text', async () => {
+        const page = await browser.newPage();
+        await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+
+        const text = await page.evaluate(() => document.body.textContent);
+
+        expect(text).toContain('Password Manager');
+    });
+});
