543f04b09cf97c28a7b0562abf2d23f87d4c995b
Žilvinas Krasauskas <zilvinas.krasauskas@unity3d.com>
2024-04-27 00:05:38 +0300

    Fix icons & make server run in background

--- UtilitiesLibrary/AssemblyInfo.cs
+++ UtilitiesLibrary/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Server")]
+[assembly: InternalsVisibleTo("Vault")]
 [assembly: InternalsVisibleTo("Tests")]
